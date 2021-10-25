89c1527dc2bf200ded2a1050e9276f21c32bc5f1
EC2 Default User <ec2-user@ip-172-31-44-214.us-east-2.compute.internal>
2021-10-25 16:00:04 +0000

    Initiver2

--- index.php
+++ index.php
@@ -1,29 +1,10 @@
-<?
-$cli = mysqli_connect('localhost','root','','users');
-
-
-
-if($_GET['name']){
-    $name = $_GET['name'];
-    $result = mysqli_query($cli,"select * from `table1` where `name` = '$name'");
-
-    while ($row = mysqli_fetch_assoc($result)){
-        $us['name'] = $row['name'];
-        $us['surname'] = $row['surname'];
-    }
-    echo 'Adi: ' . $us['name'] . '<br>Soyadi: ' . $us['surname'];
-
-
-}
-?>
-
 <html>
 <head>
 Website 001
 </head>
 <body>
 <center>
-<h2>Testing</h2>
+<h2><font color=red>Testing</font></h2>
 <p><font color=blue>Hello world!</font></p>
 </center>
 </body>
